baf148774b63cd926fa88906dac9096533f17c82
Mehmet Ozan Turhan <m.ozanturhan@hotmail.com>
2020-12-17 11:29:44 +0300

    Update webpack.config.js

--- example/core/webpack.config.js
+++ example/core/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 
-// these configs are only will affect app-core codes
-// if you want to change something for app-* use next.config instead
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, './src/index.js'),
